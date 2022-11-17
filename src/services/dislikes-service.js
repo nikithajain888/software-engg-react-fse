73f23086367b92d59fea047695fc556c63e2f431
nikithajain888 <nikithajain888@github.com>
2022-11-17 15:38:05 -0500

    updated dislikes-service

---
+++ src/services/dislikes-service.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000";
+const USERS_API = `${BASE_URL}/api/users`;
+
+const api = axios.create({
+   withCredentials: true
+});
+
+export const userTogglesTuitDislikes = async (uid, tid) =>
+   api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
+      .then(response => response.data);
+
+/**
+ * 
+ * @param {string} uid user id
+ * @param {string} tid tuit id
+ * @returns if a user has disliked the tuit, it returns a dislike object
+ */
+export const userHasDislikedTuit = async (uid, tid) => {
+   const response = await api.get(`${USERS_API}/${uid}/userDisliked/${tid}`);
+   return response.data;
+
+}
+
+export const userDislikedCount = (uid, tid) =>
+   api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
+      .then(response => response.data);
+
+export const findAllTuitsDislikedByUser = (userId) =>
+   api.get(`${USERS_API}/${userId}/dislikes`)
+      .then(response => response.data);
